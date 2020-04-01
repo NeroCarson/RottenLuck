50edc133cc7f246908b874c7823121323be6ed61
NeroCarson <55407930+NeroCarson@users.noreply.github.com>
2020-04-01 13:25:23 -0400

    Added Scripts and temp char

---
+++ RottenLuckGame/Assets/Scripts/PlayerMove.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMove : MonoBehaviour
+{
+    public float speed;
+    private Rigidbody2D playerBody;
+    private Vector3 change;
+    // Start is cal led before the first frame update
+    void Start()
+    {
+        playerBody = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //reset our change variable to zero
+        change = Vector3.zero;
+        //get our x and y inputs from player if they're moving
+        //if we get axis raw instead the we get a more Nintendo feel
+
+        change.x = Input.GetAxisRaw("Horizontal");
+        change.y = Input.GetAxisRaw("Vertical");
+
+        //change.x = Input.GetAxis("Horizontal");
+        //change.y = Input.GetAxis("Vertical");
+
+        //if method to check if there is a change to our movement
+        if (change != Vector3.zero) {
+            CharacterMove();
+        }
+
+        //log our change so we know it's moving and help trace errors
+        Debug.Log(change);
+    }
+
+    void CharacterMove() {
+        playerBody.MovePosition(
+            transform.position + change * speed * Time.deltaTime
+            );
+    }
+
+}
